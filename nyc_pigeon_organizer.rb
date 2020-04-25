d1c3036ab92048c672ff97f75c3b501be4e4c0c3
Logan McGuire <lrmcguire93@gmail.com>
2020-04-25 19:46:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,72 @@
+require "pry";
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_data = {}; #thing to be returned if necessary
+   data.each_pair { |category, cat_value| 
+    cat_value.each_pair { |specifier, names|
+      names.each { |name|
+        #data_portion = { name.to_s => { category.to_sym => [specifier.to_s]}};
+        if organized_data[name.to_s] == nil 
+          organized_data[(name.to_s)] = { category.to_sym => [specifier.to_s]};
+        elsif organized_data[name.to_s][category.to_sym] == nil
+          organized_data = organized_data[name.to_s].store({ category.to_sym => [specifier.to_s]}); 
+        else 
+          placement = organized_data[name.to_s][category.to_sym].length 
+          organized_data[name.to_s][category.to_sym][placement]= specifier.to_s; 
+        end
+        binding.pry;
+      }
+    }
+  }
+  
+    
+    
+    
+    
+    
+#    category.each_pair { |specifier, names| 
+#      names.map { |name| 
+#        data_portion = { name.to_sym => {
+#          category.to_sym => [specifier.to_s]
+#          }
+#        }
+#      organized_data.merge(data_portion);
+#      }
+        
+  
+#    category.each_key { |specifier| 
+#      specifier. { |name|
+#        data_portion = { name.to_sym => {
+ #         category.to_sym => [
+ #           specifier.to_s
+#          ]
+#        }} 
+#        organized_data.merge(data_portion);
+#      }
+#    }
+#  }
+
+  organized_data;
 end
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+nyc_pigeon_organizer(pigeon_data);
+
